1ca3eb65bc30cc2ccd472594465c2f820872336b
suncheng11 <suncheng11@meituan.com>
2025-01-20 23:15:50 +0800

    again

---
+++ mac-arm64/chapter1/mbr.asm
@@ -0,0 +1,20 @@
+;simple_mbr
+
+bits 16
+org 0x7c00
+
+start:
+    ;利用10中断打印
+    mov ah, 0x0e
+    mov al, 'X'
+    int 0x10
+  
+;死循环阻止程序退出
+hang:
+    jmp hang;
+
+;字节填充
+times 510 - ($-$$) db 0;
+
+;魔数（扇区签名）
+dw 0xAA55;
